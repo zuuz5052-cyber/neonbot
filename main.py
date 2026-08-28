9529d4f42218a574d59213b88631a9853db7ca6b
zuuz5052-cyber <zuuz5052@gmail.com>
2026-08-29 00:31:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 import threading
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
+
 # 1. Твой токен
 TOKEN = '8836477860:AAE3bN5zTLO0YZUMYleqTMbJwlXdHf1cHwI'
 bot = telebot.TeleBot(TOKEN)
 
-# 2. Твои ссылки на игры (замени на свои tiiny.site)
+# 2. Твои ссылки на игры
 URL_NEON_GAME = "https://neongamesnownb.tiiny.site" 
 URL_TRACK_GAME = "https://trackdeathgame.tiiny.site" 
-URL_WORDS_GAME = "https://worldsgame.tiiny.site"  # <--- Ссылка на игру в слова
+URL_WORDS_GAME = "https://worldsgame.tiiny.site" 
+URL_WAVE_GAME = "https://wawegame.tiiny.site"  # <--- Ссылка на игру "Волна"
 
 def get_main_keyboard():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     
     btn_neon = KeyboardButton("⚡ Неон", web_app=WebAppInfo(url=URL_NEON_GAME))
     btn_track = KeyboardButton("🏎 Трасса смерти", web_app=WebAppInfo(url=URL_TRACK_GAME))
-    btn_words = KeyboardButton("💬 Игра в слова", web_app=WebAppInfo(url=URL_WORDS_GAME)) # <--- Новая кнопка
+    btn_words = KeyboardButton("💬 Игра в слова", web_app=WebAppInfo(url=URL_WORDS_GAME))
+    btn_wave = KeyboardButton("🌊 Волна", web_app=WebAppInfo(url=URL_WAVE_GAME))
     
-    # Добавляем все три кнопки в клавиатуру
-    markup.add(btn_neon, btn_track, btn_words)
+    # Добавляем все четыре кнопки в клавиатуру
+    markup.add(btn_neon, btn_track, btn_words, btn_wave)
     return markup
 
 @bot.message_handler(commands=['start'])
@@ -33,6 +36,8 @@ def start_cmd(message):
     )
 
 print("Бот успешно запущен! Жду команд...")
+
+# Веб-сервер для предотвращения засыпания бота на Render
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
@@ -45,4 +50,5 @@ def run_server():
     server.serve_forever()
 
 threading.Thread(target=run_server, daemon=True).start()
+
 bot.polling(none_stop=True)
